09fc686879d241d2831ea29a3bb393c955f11402
haru.kirameki <lnxhrm.w02@gmail.com>
2021-04-18 06:00:29 +0000

    kadai-shusei

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
   def index
-      @tasks = Task.all
+      @task = Task.all
   end
 
   def show
-    @tasks = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def new
